500c2f5dd066644d780639fd5d46f6da47d9496d
mosePark <58103517+mosePark@users.noreply.github.com>
2024-06-07 13:07:32 +0900

    Create 24-06-07-AL.sh

---
+++ Deep-learning/active-learning/24-06-07-AL.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=AL
+#SBATCH -o SLURM.%N.%j.out
+#SBATCH -e SLURM.%N.%j.err
+#SBATCH --nodes=001
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=gpu1
+
+hostname
+date
+
+module add ANACONDA/2020.11
+
+/home1/mose1103/anaconda3/envs/fine-koactive/bin/python 24-06-07-AL.py
